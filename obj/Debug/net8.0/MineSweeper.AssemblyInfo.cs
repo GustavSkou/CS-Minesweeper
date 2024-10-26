a7c6a02def16e297b8d43ea3f5f60bd8d19c3110
GustavSkou <Gustav.skou.rasmussen@gmail.com>
2024-10-26 15:41:13 +0200

    merge

--- obj/Debug/net8.0/MineSweeper.AssemblyInfo.cs
+++ obj/Debug/net8.0/MineSweeper.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MineSweeper")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2a362e7aed6bdd151d8885baa43bfb1bb183f6a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff8c43e0380e4739033f166986136ac0719938b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("MineSweeper")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MineSweeper")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
