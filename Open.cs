264fcc137077aca671e447323ab85c57281dec2c
GustavSkou <Gustav.skou.rasmussen@gmail.com>
2024-10-29 12:31:45 +0100

    working opening surrounding

--- Open.cs
+++ Open.cs
@@ -31,37 +31,53 @@ class Open : Cell
                     continue;
                 }
 
-                if (mineField[this.row + row, this.column + column] is Mine)
-                {
-                    surroundingMinesCounter++;
-                }
-            }
-        }
-
-        if (surroundingMines == 0)
-        {
-            for (int row = -1; row <= 1; row++)
-            {
-                if (this.row + row < 0 || this.row + row > mineField.GetLength(0)-1)
+                if (row == 0 && column == 0)
                 {
                     continue;
                 }
 
-                for (int column = -1; column <= 1; column++)
+                if (mineField[this.row + row, this.column + column] is Mine)
                 {
-                    if (this.column + column < 0 || this.column + column > mineField.GetLength(1)-1)
-                    {
-                        continue;
-                    }
+                    surroundingMinesCounter++;
+                }       
+            }
+        }
 
-                    if (!(mineField[this.row + row, this.column + column] is Open))
-                    {
-                        mineField[this.row + row, this.column + column].Open(mineField);  
-                    }
-                }
-            }    
+        if (surroundingMinesCounter == 0)
+        {
+            OpenSurroundingCells(mineField);
         }
 
         return surroundingMinesCounter;
+    }
+
+    private void OpenSurroundingCells(Cell[,] mineField)
+    {
+        List<Cell> cellsToOpen = new List<Cell>();
+
+        for (int row = -1; row <= 1; row++)
+        {
+            if (this.row + row < 0 || this.row + row > mineField.GetLength(0)-1)
+            {
+                continue;
+            }
+
+            for (int column = -1; column <= 1; column++)
+            {
+                if (this.column + column < 0 || this.column + column > mineField.GetLength(1)-1)
+                {
+                    continue;
+                }
+
+                if (row == 0 && column == 0)
+                {
+                    continue;
+                }
+
+                cellsToOpen.Add(mineField[row,column]);
+                cellsToOpen[0].Open(mineField);
+                cellsToOpen.RemoveAt(0);
+            }
+        }
     }
 }
--- obj/Debug/net8.0/MineSweeper.AssemblyInfo.cs
+++ obj/Debug/net8.0/MineSweeper.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MineSweeper")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a279f7b6f25b42a98116f3ad585f04a09d64691f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c23b0df067a88bedb3bfac0e034fb9d837e51759")]
 [assembly: System.Reflection.AssemblyProductAttribute("MineSweeper")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MineSweeper")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
