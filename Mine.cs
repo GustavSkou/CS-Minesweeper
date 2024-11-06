b8336e1c33471ce37211bda5981547b8d03b971a
GustavSkou <Gustav.skou.rasmussen@gmail.com>
2024-11-06 13:30:39 +0100

    refacor

--- Mine.cs
+++ Mine.cs
@@ -1,26 +1,13 @@
 class Mine : Cell
 {
-    private string mineModel = "¤";
     public Mine(int row, int column) : base(row, column)
     {
         this.row = row;
         this.column = column;
     }
 
-    public override string GetChar()
-    {
-        if (!isOpen)
-        {
-            return base.GetChar();
-            
-        }
-        return mineModel;
-    }
-
     public override void Open(Cell[,] minefield)
     {
-        isOpen = true;
-        GetChar();
-        Console.WriteLine("boom");
+        model = "¤";
     }
 }
--- Open.cs
+++ Open.cs
@@ -5,16 +5,13 @@ class Open : Cell
         this.row = row;
         this.column = column;
         surroundingMines = SetsurroundingMines(mineField);
-    }
-
-    public override string GetChar()
-    {
-        return surroundingMines.ToString();
+        model = surroundingMines.ToString();
     }
 
     private int SetsurroundingMines(Cell[,] mineField)
     {
         int surroundingMinesCounter = 0;
+
         for (int row = -1; row <= 1; row++)
         {
             if (this.row + row < 0 || this.row + row > mineField.GetLength(0)-1)
@@ -40,48 +37,7 @@ class Open : Cell
                 }       
             }
         }
+
         return surroundingMinesCounter;
     }
-
-    /*private void OpenSurroundingCells(List<Cell> cellsToOpen, Cell[,] mineField)
-    {
-        for (int row = -1; row <= 1; row++)
-        {
-            if (this.row + row < 0 || this.row + row > mineField.GetLength(0)-1) //Out of bounds
-            {
-                continue;
-            }
-
-            for (int column = -1; column <= 1; column++)
-            {
-                if (this.column + column < 0 || this.column + column > mineField.GetLength(1)-1) //Out of bounds
-                {
-                    continue;
-                }
-
-                if (row == 0 && column == 0) //it self
-                {
-                    continue;
-                }
-
-                if (mineField[this.row + row, this.column + column] is Open)
-                {
-                    continue;
-                }
-
-                if (mineField[this.row + row, this.column + column].isOpen)
-                {
-                    continue;
-                }
-
-                mineField[this.row + row, this.column + column].isOpen = true;
-                cellsToOpen.Add(mineField[this.row + row, this.column + column]);
-            }
-        }
-
-        for (int index = 0; index < cellsToOpen.Count(); index++)
-        {
-            cellsToOpen[index].Open(mineField);
-        }
-    }*/
 }
