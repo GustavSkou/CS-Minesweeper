44a5af9530eb96bab7f6ebbbc2cac3b4506c077a
GustavSkou <Gustav.skou.rasmussen@gmail.com>
2024-11-06 13:27:47 +0100

    refactor

--- Program.cs
+++ Program.cs
@@ -6,17 +6,14 @@
         Player player = new Player(mineField);
 
         mineField.LayMines(5);
-        mineField.PrintMineField();
-
-        //mineField.getMineField()[1,1].Open(mineField.getMineField());
-        mineField.PrintMineField();
+        mineField.PrintMineField(null);
 
         while (true)
         {
             int[] coordinets = player.GetInput();
 
-            mineField.getMineField()[coordinets[0], coordinets[1]].
-                Open(mineField.getMineField());
+            mineField.Field[coordinets[0], coordinets[1]].
+                Open(mineField.Field);
                 
             mineField.PrintMineField(coordinets);
         }
